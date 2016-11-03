c9a2f55955d65be505769db0be33b0146d8ca0d8
Dmitry Kalashnik <dkalashnik@mirantis.com>
2016-11-03 17:39:05 +0300

    Add default values for config gen

--- config/fuel_config.py
+++ config/fuel_config.py
@@ -3,14 +3,15 @@ import json
 from clients.system import general_client
 
 
-def get_nailgun_config(address, username, password, key_file, key):
+def get_nailgun_config(address, username, password, key_file=None, key=None):
     ssh = general_client.GeneralActionsClient(address, username, password)
     ssh.put_file("fixtures/hiera", "/tmp/hiera")
-    ssh.execute("chmod +x /tmp/hiera")
+    ssh.execute(["chmod", "+x", "/tmp/hiera"])
 
-    exit_status, stdout, stderr = ssh.execute(["/tmp/hiera", "FUEL_ACCESS"])
+    exit_status, stdout, stderr = ssh.execute(
+        ["/tmp/hiera", "--format", "json", "FUEL_ACCESS"])
     print stdout
-    print json.load(stdout)
+    print json.loads(stdout)
 
 
 def get_nodes_from_nailgun():
@@ -27,3 +28,5 @@ def get_lma_credentials():
 
 def main():
     get_nailgun_config("10.109.0.2", "root", "r00tme")
+
+main()
