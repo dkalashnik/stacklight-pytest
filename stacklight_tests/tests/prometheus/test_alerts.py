b4ea58eb7466f6189fb576c8919d26b7d7d744a6
vgusev <vgusev@mirantis.com>
2017-05-11 15:23:43 +0400

    Add tests for alerts for kubernetes services

--- stacklight_tests/tests/prometheus/test_alerts.py
+++ stacklight_tests/tests/prometheus/test_alerts.py
@@ -1,3 +1,6 @@
+import pytest
+
+
 class TestPrometheusAlerts(object):
     def test_system_load_alerts(self, cluster, prometheus_alerting):
         """Check that alert for load overage and idle on node can be fired.
@@ -32,3 +35,35 @@ class TestPrometheusAlerts(object):
         with compute.os.make_temporary_load(load_processes_count):
             check_status()
         check_status(is_fired=False)
+
+
+class TestKubernetesAlerts(object):
+    @pytest.mark.parametrize(
+        "service", ["kube-proxy", "kube-apiserver", "kube-controller-manager",
+                    "kubelet", "kube-scheduler"])
+    def test_kubernetes_alert(self, cluster, prometheus_alerting, service):
+        """Check that alerts for kubernetes services can be fired.
+
+        Scenario:
+            1. Check that alert is not fired
+            2. Stop the corresponding kubernetes service on controller node
+            3. Wait until and check that alert was fired
+            4. Start the kubernetes service
+            5. Wait until and check that alert was ended
+
+        Duration 30m
+        """
+        ctl = [host for host in cluster.hosts
+               if host.fqdn.startswith("ctl")][0]
+        criteria = {
+            "name": "ProcstatRunningKubernetes",
+            "host": ctl.hostname,
+        }
+        prometheus_alerting.check_alert_status(
+            criteria, is_fired=False, timeout=6 * 60)
+        ctl.os.manage_service(service, "stop")
+        prometheus_alerting.check_alert_status(
+            criteria, is_fired=True, timeout=6 * 60)
+        ctl.os.manage_service(service, "start")
+        prometheus_alerting.check_alert_status(
+            criteria, is_fired=False, timeout=6 * 60)
